38694813748cbbd1271773c48a11ec9937fdc6d1
Annelís <juanysacalxot376@gmail.com>
2022-03-29 19:17:52 -0600

    Diseño de ventana servidor y cliente

--- src/main/java/socket/Cliente.java
+++ src/main/java/socket/Cliente.java
@@ -1,4 +1,21 @@
 package socket;
 
-public class Cliente {
+import java.util.Observable;
+
+public class Cliente extends Observable implements Runnable {
+
+    private int puerto;
+
+    //constructor
+    public Cliente(int puerto){
+        this.puerto = puerto;
+    }
+
+    @Override
+    public void run() {
+
+        //IP
+        final String HOST = "";
+
+    }
 }
